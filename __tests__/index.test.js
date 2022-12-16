c317520a46b772775fbfa970441ea2fd761c87a2
lewqs <lewisbrown3108@gmail.com>
2022-12-16 00:21:55 +0000

    Updated variable names to make them clearer on what they are

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,43 +1,47 @@
 const updateRemoteStudents = require("../index");
 
 describe("updateRemoteStudents", () => {
-  test("returns empty array when not passed array", () => {
+  test("returns empty array when passed empty array", () => {
     expect(updateRemoteStudents([])).toEqual([]);
   });
   test("returns a new array", function () {
-    let inputArray = [
+    const students = [
       { name: "Hypatia", age: 31, location: "leeds" },
       { name: "Ramanujan", age: 22 },
       { name: "Tao", age: 47, location: "manchester" },
     ];
-    expect(updateRemoteStudents(inputArray)).not.toBe(inputArray);
+    const copyStudents = [
+      { name: "Hypatia", age: 31, location: "leeds" },
+      { name: "Ramanujan", age: 22 },
+      { name: "Tao", age: 47, location: "manchester" },
+    ];
+    expect(updateRemoteStudents(students)).not.toBe(copyStudents);
   });
   test("does not mutate the input array", function () {
-    let inputArray = [
+    const students = [
       { name: "Hypatia", age: 31, location: "leeds" },
       { name: "Ramanujan", age: 22 },
       { name: "Tao", age: 47, location: "manchester" },
     ];
-    let copyInputArray = [
+    const copyStudents = [
       { name: "Hypatia", age: 31, location: "leeds" },
       { name: "Ramanujan", age: 22 },
       { name: "Tao", age: 47, location: "manchester" },
     ];
-    updateRemoteStudents(inputArray);
-    expect(inputArray).toEqual(copyInputArray);
+    updateRemoteStudents(students);
+    expect(students).toEqual(copyStudents);
   });
   test("inserts 'remote' for each element that do not have a location property", function () {
-    let inputArray = [
+    const inputStudents = [
       { name: "Hypatia", age: 31, location: "leeds" },
       { name: "Ramanujan", age: 22 },
       { name: "Tao", age: 47, location: "manchester" },
     ];
-    let output = [
+    const outputStudents = [
       { name: "Hypatia", age: 31, location: "leeds" },
       { name: "Ramanujan", age: 22, location: "remote" },
       { name: "Tao", age: 47, location: "manchester" },
     ];
-    updateRemoteStudents(inputArray);
-    expect(updateRemoteStudents(inputArray)).toEqual(output);
+    expect(updateRemoteStudents(inputStudents)).toEqual(outputStudents);
   });
 });
